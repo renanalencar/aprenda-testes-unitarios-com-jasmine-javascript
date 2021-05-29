8b8ae3ceed5392bb388cadf86d756cf9e56920c4
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-05-29 20:29:46 -0300

    toBeDefined

---
+++ spec/toBeDefinedSpec.js
@@ -0,0 +1,12 @@
+describe("Teste do toBeDefined", function () {
+    it("Deve demonstrar o uso do toBeDefined", function() {
+        var n1 = 10;
+        var n2;
+        var n3 = undefined;
+        expect(n1).toBeDefined();
+        expect(null).toBeDefined();
+        expect(NaN).toBeDefined();
+        expect(n2).not.toBeDefined();
+        expect(n3).not.toBeDefined();
+    }); 
+});
