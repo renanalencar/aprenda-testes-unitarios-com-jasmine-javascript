2cd8f2be5029a6e8723ee9791d905b5e52423fec
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-05-30 12:46:43 -0300

    and.returnValues

--- spec/spySpec.js
+++ spec/spySpec.js
@@ -11,7 +11,8 @@ describe("Testes do objeto Spy", function() {
 
     beforeAll(function() {
         // spyOn(Calculadora, "somar").and.callThrough();
-        spyOn(Calculadora, "somar").and.returnValue(10);
+        // spyOn(Calculadora, "somar").and.returnValue(10);
+        spyOn(Calculadora, "somar").and.returnValues(1, 5);
         spyOn(Calculadora, "subtrair");
     });
 
@@ -43,7 +44,13 @@ describe("Testes do objeto Spy", function() {
     //     expect(Calculadora.subtrair(2, 1)).toBeUndefined();
     // });
 
+    // it("deve retornar 10 para o método somar", function() {
+    //     expect(Calculadora.somar(1,2)).toEqual(10);
+    // });
+
     it("deve retornar 10 para o método somar", function() {
-        expect(Calculadora.somar(1,2)).toEqual(10);
-    })
+        expect(Calculadora.somar(3,4)).toEqual(1);
+        expect(Calculadora.somar(1,2)).toEqual(5);
+        expect(Calculadora.somar(4,4)).toBeUndefined();
+    });
 });
