55435965d6ebbbdef8fa4ee56aab81d072af0948
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-05-30 15:50:32 -0300

    jasmine.arrayContaining

--- spec/jasmineSpec.js
+++ spec/jasmineSpec.js
@@ -5,6 +5,7 @@ describe("Testes do objeto jasmine", function () {
     beforeAll(function () {
         dobro = jasmine.createSpy("dobro");
         carro = { 'ano': 2017 };
+        numeros = [1, 2, 3, 4, 5];
     });
 
     it("deve demonstrar o uso do jasmine.any", function () {
@@ -25,5 +26,12 @@ describe("Testes do objeto jasmine", function () {
         expect(carro).toEqual(jasmine.objectContaining({ 'ano': 2017 }));
 
         expect(carro).not.toEqual(jasmine.objectContaining({ 'ano': 2016 }));
+    });
+
+    it("deve demonstrar o uso do jasmine.arrayContaining", function () {
+        expect(numeros).toEqual(jasmine.arrayContaining([3]));
+        expect(numeros).toEqual(jasmine.arrayContaining([2, 4]));
+
+        expect(carro).not.toEqual(jasmine.arrayContaining([6]));
     });
 });
