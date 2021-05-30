d109368068629ecf5f6b3ca915bd636ea7e2c5cd
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-05-30 12:41:21 -0300

    and.returnValue

--- spec/spySpec.js
+++ spec/spySpec.js
@@ -10,35 +10,40 @@ describe("Testes do objeto Spy", function() {
     };
 
     beforeAll(function() {
-        spyOn(Calculadora, "somar").and.callThrough();
+        // spyOn(Calculadora, "somar").and.callThrough();
+        spyOn(Calculadora, "somar").and.returnValue(10);
         spyOn(Calculadora, "subtrair");
     });
 
-    xit("deve possuir o método somar como não definido", function() {
-        expect(Calculadora.somar(1, 1)).toBeUndefined();
-    });
+    // it("deve possuir o método somar como não definido", function() {
+    //     expect(Calculadora.somar(1, 1)).toBeUndefined();
+    // });
 
-    xit("deve chamar o método somar ao menos uma vez", function() {
-        expect(Calculadora.somar).not.toHaveBeenCalled();
-        Calculadora.somar(1, 1);
-        expect(Calculadora.somar).toHaveBeenCalled();
-    });
+    // it("deve chamar o método somar ao menos uma vez", function() {
+    //     expect(Calculadora.somar).not.toHaveBeenCalled();
+    //     Calculadora.somar(1, 1);
+    //     expect(Calculadora.somar).toHaveBeenCalled();
+    // });
 
-    xit("deve chamar o método somar duas vezes", function() {
-        Calculadora.somar(1, 1);
-        Calculadora.somar(1, 2);
-        expect(Calculadora.somar).toHaveBeenCalledTimes(2);
-    });
+    // it("deve chamar o método somar duas vezes", function() {
+    //     Calculadora.somar(1, 1);
+    //     Calculadora.somar(1, 2);
+    //     expect(Calculadora.somar).toHaveBeenCalledTimes(2);
+    // });
 
-    it("deve chamar o método somar com os parâmetros válidos", function() {
-        Calculadora.somar(1, 1);
-        Calculadora.somar(1, 2);
-        expect(Calculadora.somar).toHaveBeenCalledWith(1,1);
-        expect(Calculadora.somar).toHaveBeenCalledWith(1,2);
-    });
+    // it("deve chamar o método somar com os parâmetros válidos", function() {
+    //     Calculadora.somar(1, 1);
+    //     Calculadora.somar(1, 2);
+    //     expect(Calculadora.somar).toHaveBeenCalledWith(1,1);
+    //     expect(Calculadora.somar).toHaveBeenCalledWith(1,2);
+    // });
 
-    it("deve executar o método somar original", function() {
-        expect(Calculadora.somar(1, 1)).toEqual(2);
-        expect(Calculadora.subtrair(2, 1)).toBeUndefined();
-    });
+    // it("deve executar o método somar original", function() {
+    //     expect(Calculadora.somar(1, 1)).toEqual(2);
+    //     expect(Calculadora.subtrair(2, 1)).toBeUndefined();
+    // });
+
+    it("deve retornar 10 para o método somar", function() {
+        expect(Calculadora.somar(1,2)).toEqual(10);
+    })
 });
