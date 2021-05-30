98a8195cf4067423caca0312c97b630e77351aa3
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-05-30 15:29:36 -0300

    jasmine.createSpy

---
+++ spec/jasmineSpec.js
@@ -0,0 +1,15 @@
+describe("Testes do objeto jasmine.createSpy", function() {
+    
+    var somar;
+
+    beforeAll(function() {
+        somar = jasmine.createSpy("somar");
+    });
+
+    it("deve demonstrar o uso do jasmine.createSpy", function() {
+        somar(1, 2);
+
+        expect(somar).toHaveBeenCalled();
+        expect(somar).toHaveBeenCalledWith(1, 2);
+    });
+});
