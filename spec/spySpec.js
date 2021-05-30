f449330937ef8d6dc3844854f0e59158adb86252
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-05-29 22:46:36 -0300

    and.callThrough

--- spec/spySpec.js
+++ spec/spySpec.js
@@ -3,11 +3,15 @@ describe("Testes do objeto Spy", function() {
     var Calculadora = {
         somar: function(n1, n2) {
             return n1 + n2;
+        },
+        subtrair: function(n1, n2) {
+            return n1 - n2;
         }
     };
 
     beforeAll(function() {
-        spyOn(Calculadora, "somar");
+        spyOn(Calculadora, "somar").and.callThrough();
+        spyOn(Calculadora, "subtrair");
     });
 
     xit("deve possuir o método somar como não definido", function() {
@@ -20,7 +24,7 @@ describe("Testes do objeto Spy", function() {
         expect(Calculadora.somar).toHaveBeenCalled();
     });
 
-    it("deve chamar o método somar duas vezes", function() {
+    xit("deve chamar o método somar duas vezes", function() {
         Calculadora.somar(1, 1);
         Calculadora.somar(1, 2);
         expect(Calculadora.somar).toHaveBeenCalledTimes(2);
@@ -31,5 +35,10 @@ describe("Testes do objeto Spy", function() {
         Calculadora.somar(1, 2);
         expect(Calculadora.somar).toHaveBeenCalledWith(1,1);
         expect(Calculadora.somar).toHaveBeenCalledWith(1,2);
+    });
+
+    it("deve executar o método somar original", function() {
+        expect(Calculadora.somar(1, 1)).toEqual(2);
+        expect(Calculadora.subtrair(2, 1)).toBeUndefined();
     });
 });
